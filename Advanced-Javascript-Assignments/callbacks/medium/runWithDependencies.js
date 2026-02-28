7081f6b506989c5185c3a63db6d2d2db79625c08
abhicsng007 <abhicsng007@gmail.com>
2026-02-28 12:48:50 +0530

    solved runWithDependencies

--- Advanced-Javascript-Assignments/callbacks/medium/runWithDependencies.js
+++ Advanced-Javascript-Assignments/callbacks/medium/runWithDependencies.js
@@ -11,6 +11,61 @@
 // Invoke finalCallback after all tasks have completed, or with an error
 // if any task fails.
 
-function runWithDependencies(tasks, finalCallback) {}
+function runWithDependencies(tasks, finalCallback) {
+    const taskMap = new Map();
+    const dependents = new Map();
+    const inDegree = new Map();
+    const results = {};
+    let completed = 0;
+    let finished = false;
+
+    for(const task of tasks){
+        taskMap.set(task.id,task);
+        inDegree.set(task.id,task.deps.length);
+        dependents.set(task.id,[]);
+    }
+
+    for(const task of tasks){
+        for(const dep of task.deps){
+            dependents.get(dep).push(task.id);
+        }
+    }
+
+    function runTask(id){
+        if(finished) return;
+        const task = taskMap.get(id);
+
+        task.run((err,result) => {
+            if(finished) return;
+
+            if(err){
+                finished = true;
+                return finalCallback(err);
+            }
+
+            results[id] = result;
+            completed++;
+
+            for(const dependentsId of dependents.get(id)){
+                inDegree.set(dependentsId,inDegree.get(dependentsId)-1);
+
+                if(inDegree.get(dependentsId) === 0){
+                    runTask(dependentsId);
+                }
+            }
+
+            if(completed === tasks.length){
+                finished = true;
+                return finalCallback(err,results);
+            }
+        });
+    }
+
+    for(const task of tasks){
+        if(inDegree.get(task.id) === 0){
+            runTask(task.id);
+        }
+    }
+}
 
 module.exports = runWithDependencies;
